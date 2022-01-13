333c2951551d610e4535d82fb24c2f0bd1b9d28a
Will Pine <wpine215@gmail.com>
2022-01-13 03:49:08 -0500

    WIP overhaul

--- flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_StateInitializer.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_StateInitializer.js
@@ -1,47 +1,52 @@
 class A32NX_StateInitializer {
-    init() {
-        const active = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_ACTIVE", "Bool");
-        if (active === 1) {
-            const autobrakeLevel = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_AUTOBRK_LVL", "Number");
-            const autoThrustActive = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_AUTOTHRUST_ACTIVE", "Bool");
-            const isManaged = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_USE_MANAGED_SPEED", "Bool");
-            const selectedSpeed = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_SELECTED_SPEED", "Number");
-            const isApproach = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_IS_APPROACH", "Bool");
-
-            if (autoThrustActive === 1) {
-                this.setThrottlePositionToClimb().then(() => {
-                    SimVar.SetSimVarValue("K:AUTO_THROTTLE_ARM", "Number", 1);
-                });
-            }
-
-            if (autobrakeLevel === 1) {
-                SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_LOW_ON_IS_PRESSED", "Number", 1);
-            } else if (autobrakeLevel === 2) {
-                SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_MED_ON_IS_PRESSED", "Number", 1);
-            } else if (autobrakeLevel === 3) {
-                SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_MAX_ON_IS_PRESSED", "Number", 1);
-            }
-
-            if (isApproach) {
-                SimVar.SetSimVarValue("L:A32NX_FMGC_FLIGHT_PHASE", "Number", 5);
-            }
-
-            if (isManaged) {
-                SimVar.SetSimVarValue("H:A320_Neo_FCU_SPEED_PUSH", "Number", 1);
-            } else {
-                SimVar.SetSimVarValue("L:A320_Neo_FCU_SPEED_SET_DATA", "Number", selectedSpeed).then(() => {
-                    SimVar.SetSimVarValue("H:A320_Neo_FCU_SPEED_SET", "Number", 1).then(() => {
-                        SimVar.SetSimVarValue("H:A320_Neo_FCU_SPEED_PULL", "Number", 1);
-                    });
-                });
-            }
-
-            SimVar.SetSimVarValue("K:AUTO_THROTTLE_ARM", "Number", 1);
-        }
+    constructor() {
+        this.autobrakeLevel = null;
+        this.isManaged = null;
+        this.selectedSpeed = null;
     }
 
-    setThrottlePositionToClimb() {
-        SimVar.SetSimVarValue("L:A32NX_AUTOTHRUST_TLA:1", "Number", 25);
-        SimVar.SetSimVarValue("L:A32NX_AUTOTHRUST_TLA:2", "Number", 25);
+    init() {
+        this.autobrakeLevel = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_AUTOBRK_LVL", "Number");
+        this.isManaged = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_USE_MANAGED_SPEED", "Bool");
+        this.selectedSpeed = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_SELECTED_SPEED", "Number");
+    }
+
+    update() {
+        const active = SimVar.GetSimVarValue("L:A32NX_STATE_INIT_ACTIVE", "Bool");
+        console.log("HEREEEE: active=", active);
+        if (active === 1) {
+            const athr = SimVar.GetSimVarValue("L:A32NX_AUTOTHRUST_STATUS", "Number");
+
+            if (athr !== 2) {
+                SimVar.SetSimVarValue("K:AUTO_THROTTLE_ARM", "number", 1);
+                SimVar.SetSimVarValue("L:A32NX_AUTOTHRUST_TLA:1", "Number", 25);
+                SimVar.SetSimVarValue("L:A32NX_AUTOTHRUST_TLA:2", "Number", 25);
+
+                if (this.autobrakeLevel === 1) {
+                    SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_LOW_ON_IS_PRESSED", "Number", 1).then(() => {
+                        SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_LOW_ON_IS_PRESSED", "Number", 0);
+                    });
+                } else if (this.autobrakeLevel === 2) {
+                    SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_MED_ON_IS_PRESSED", "Number", 1).then(() => {
+                        SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_MED_ON_IS_PRESSED", "Number", 0);
+                    });
+                } else if (this.autobrakeLevel === 3) {
+                    SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_MAX_ON_IS_PRESSED", "Number", 1).then(() => {
+                        SimVar.SetSimVarValue("L:A32NX_OVHD_AUTOBRK_MAX_ON_IS_PRESSED", "Number", 0);
+                    });
+                }
+                if (this.isManaged) {
+                    SimVar.SetSimVarValue("H:A320_Neo_FCU_SPEED_PUSH", "Number", 1);
+                } else {
+                    SimVar.SetSimVarValue("L:A320_Neo_FCU_SPEED_SET_DATA", "Number", this.selectedSpeed).then(() => {
+                        SimVar.SetSimVarValue("H:A320_Neo_FCU_SPEED_SET", "Number", 1).then(() => {
+                            SimVar.SetSimVarValue("H:A320_Neo_FCU_SPEED_PULL", "Number", 1);
+                        });
+                    });
+                }
+            } else {
+                SimVar.SetSimVarValue("L:A32NX_STATE_INIT_ACTIVE", "Bool", 0);
+            }
+        }
     }
 }
